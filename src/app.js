eca31babefb2c21da586eb38ee3ca5665a973f8c
Dário Cadete <cadetedario@hotmail.com>
2021-11-12 17:22:42 +0000

    MiniProjeto1

---
+++ src/app.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+  var app = new Vue({
+  el: '#app',
+  data: {
+    nome: 'Dário Cadete',
+    resumo_proficional: 'Formado em Ciências da Computação, possuo sólidos conhecimentos em - Analise de Sistemas; - Redes de Computadores; - Engenharia de Software; - Projectista de Software; - Engenheiro de Requisitos; - Implementação e Desenvolvimento de Sistemas; - Análise e Desenvolvimento de Bases de Dados; - Analista de Segurança em Sistemas Informáticos. Experiência de 4 anos na área de Inquéritos de Produção Agrícola.'
+  }
+})
+
+}
